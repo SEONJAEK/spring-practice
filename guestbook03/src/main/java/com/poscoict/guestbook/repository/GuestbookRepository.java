d9e9855185acabc905cb25b7d24be00efc056c9d
bit <bit@DESKTOP-N5AI91N>
2022-01-28 16:43:12 +0900

    수정

--- guestbook03/src/main/java/com/poscoict/guestbook/repository/GuestbookRepository.java
+++ guestbook03/src/main/java/com/poscoict/guestbook/repository/GuestbookRepository.java
@@ -149,8 +149,8 @@ public class GuestbookRepository {
 	private Connection getConnection() throws SQLException {
 		Connection conn = null;
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mysql://192.168.0.52:3307/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
